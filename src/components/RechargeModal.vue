985b693980a8bcb9fa49c0786c4e5e3f564a46ed
BlackTrace <1078242064@qq.com>
2018-12-12 22:48:37 +0800

    充值，提现。第一个初始版本

--- src/components/RechargeModal.vue
+++ src/components/RechargeModal.vue
@@ -1,119 +1,129 @@
 <template>
-    <el-dialog :title="$t('modal.recharge')" width="400px" :visible.sync="show">
-        <div class="result-number">{{value}} (ONT) = {{result}} (TONT)</div>
-        <el-input-number 
-            v-model="value"
-            :step="1"
-            :controls="false"
-            :precision="2"
-            :min="0"
-            :max="10000"
-            style="width: 100%">
-        </el-input-number>
-        <span slot="footer">
-            <el-button @click="close">{{$t('btn.cancel')}}</el-button>
-            <el-button type="primary" @click="submit">{{$t('btn.ok')}}</el-button>
-        </span>
-    </el-dialog>
+  <el-dialog :title="$t('modal.recharge')" width="400px" :visible.sync="show">
+    <div class="result-number">{{value}} (ONT) = {{result}} (TONT)</div>
+    <el-input-number
+      v-model="value"
+      :step="1"
+      :controls="false"
+      :precision="2"
+      :min="0"
+      :max="10000"
+      style="width: 100%"
+    ></el-input-number>
+    <span slot="footer">
+      <el-button @click="close">{{$t('btn.cancel')}}</el-button>
+      <el-button type="primary" @click="submit">{{$t('btn.ok')}}</el-button>
+    </span>
+  </el-dialog>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { toFixed, multiple} from '@/utils/util'
+import { mapGetters } from "vuex";
+import { toFixed, multiple } from "@/utils/util";
 import { client } from "ontology-dapi";
-
 
 let Ont = require("ontology-ts-sdk");
 export default {
-    data() {
-        return {
-            show: false,
-            value: 0,
-            userScriptHash:'',
-            userAddress:''
-        }
+  data() {
+    return {
+      show: false,
+      value: 0,
+      userScriptHash: "",
+      userAddress: ""
+    };
+  },
+  methods: {
+    open() {
+      this.value = 0;
+      this.show = true;
     },
-    methods: {
-        open() {
-            this.value = 0;
-            this.show = true;
-        },
-        close() {
-            this.show = close;
-        },
-        submit() {
+    close() {
+      this.show = close;
+    },
+    submit() {
+      this.GetUserInfo().then(() => {
+        this.recharge(
+          this.userScriptHash,
+          this.userScriptHash,
+          this.value
+        ).then(() => {
+          alert("1");
+        });
+      });
 
-            console.log(this.value);
-            this.GetUserInfo().then(
-                () =>{
-                    this.recharge(this.userScriptHash, this.userScriptHash, this.value).then(
-                        () => {
-                            alert("1");
-                        }
-                    )
-                }
-            );
-            
-            this.close();
-        },
-    async GetUserInfo(){
-            let Crypto = Ont.Crypto;
-            let util = Ont.utils;
-            client.registerClient({});
-            const account = await client.api.asset.getAccount(); //得到当前插件钱包的用户地址。
-            console.log(account);
-            let Address = new Crypto.Address(account);
-            console.log(Address);
-            this.userAddress = Address.toString()
-            this.userScriptHash = Address.serialize();
+      this.close();
     },
-    async recharge(fromUserScriptHash, toUserScript, amount) {
+    async GetUserInfo() {
+      let Crypto = Ont.Crypto;
+      let util = Ont.utils;
+      client.registerClient({});
+      const account = await client.api.asset.getAccount(); //得到当前插件钱包的用户地址。
+      console.log(account);
+      let Address = new Crypto.Address(account);
+      console.log(Address);
+      this.userAddress = Address.toString();
+      this.userScriptHash = Address.serialize();
+    },
+    recharge(fromUserScriptHash, toUserScript, amount) {
       //付款账户，充值给谁，充值的个数，需要修改，不能跑完成吧，需要容错处理和健壮性
-       //这里可以来一些检测，如账户的检测，是否都为20长度的字符串
+      //这里可以来一些检测，如账户的检测，是否都为20长度的字符串
 
-       const scriptHash = '95feeda3a7f41e43204353de64aa7b016e4ffaa3'; //合约的地址
-       const operation = 'Recharge';//调用合约的方法名
-       const args = [{type: 'Bytearray', value: fromUserScriptHash}, {type: 'Bytearray', value: toUserScript}, {type: 'Integer', value: amount}];
-       const gasPrice = 500;
-       const gasLimit = 200000;
-       const result = await client.api.smartContract.invoke({scriptHash, operation, args, gasPrice, gasLimit});
-       console.log(result);//大概1秒后再接下去请求。
-       setTimeout(
-           async ()=>{
-               console.log(2);
+      const scriptHash = "95feeda3a7f41e43204353de64aa7b016e4ffaa3"; //合约的地址
+      const operation = "Recharge"; //调用合约的方法名
+      const args = [
+        { type: "Bytearray", value: fromUserScriptHash },
+        { type: "Bytearray", value: toUserScript },
+        { type: "Integer", value: amount }
+      ];
+      const gasPrice = 500;
+      const gasLimit = 200000;
+      client.api.smartContract
+        .invoke({ scriptHash, operation, args, gasPrice, gasLimit })
+        .then(result => {
+          setTimeout(() => {
+            console.log(2);
             console.log(result);
-            console.log(result['transaction']);
-            let txid = result['transaction']//这里需要判断一下，是否成功什么的。
-            const txlog = await client.api.network.getSmartCodeEvent({value : txid.toString()});//得到交易的状态，json里面有个notify
-            //取txlogjson文件中的最终信息，来看，结果，然后给界面上相应
-        
-            console.log(txlog);
-           },
-           1500
-       );
-      
-    }
-    },
-    computed: {
-        ...mapGetters([
-            'user',
-            'balance',
-            'loginStatus',
-            'btType',
-            'currencys'
-        ]),
-        result() {
-            return toFixed(multiple([this.value || 0, 0.5]));
+            console.log(result["transaction"]);
+            let txid = result["transaction"]; //这里需要判断一下，是否成功什么的。
+            client.api.network.getSmartCodeEvent({
+              value: txid.toString()
+            }).then(
+                (err)=>{
+                    console.log(err);//得到交易的状态，json里面有个notify
+                    //取txlogjson文件中的最终信息，来看，结果，然后给界面上相应响应，主要看NOtify中对应的自己合约的东西。
+                    //对应自己的合约hash里面没有error的信息，就代表成功，
+                },
+                (err)=>{
+                    console.log(err);//这里可能是网络原因或者交易还没被执行，还没有结果会到这里。
+                }
+            ); 
+            
+          }, 1500);
+        },
+        (err)=>{
+            console.log(err)//发送交易执行失败走这里，或者用户取消交易导致的
+            if(err == "CANCELED"){
+                //用户在主动取消的，点击了cancel按钮的情况
+            }
         }
+        );
+
     }
-}
+  },
+  computed: {
+    ...mapGetters(["user", "balance", "loginStatus", "btType", "currencys"]),
+    result() {
+      return toFixed(multiple([this.value || 0, 0.5]));
+    }
+  }
+};
 </script>
 
 <style lang="less">
 .result-number {
-    padding-bottom: 20px;
-    font-size: 20px;
-    text-align: center;
+  padding-bottom: 20px;
+  font-size: 20px;
+  text-align: center;
 }
 </style>
 
--- src/components/WithdrawModal.vue
+++ src/components/WithdrawModal.vue
@@ -1,67 +1,133 @@
 <template>
-    <el-dialog :title="$t('modal.withdraw')" width="400px" :visible.sync="show">
-        <div class="result-number">{{value}} (TONT) = {{result}} (ONT)</div>
-        <el-input-number
-            v-model="value"
-            :step="1"
-            :controls="false"
-            :precision="2"
-            :min="0"
-            :max="10000"
-            style="width: 100%">
-        </el-input-number>
-        <span slot="footer">
-            <el-button @click="close">{{$t('btn.cancel')}}</el-button>
-            <el-button type="primary" @click="submit">{{$t('btn.ok')}}</el-button>
-        </span>
-    </el-dialog>
+  <el-dialog :title="$t('modal.withdraw')" width="400px" :visible.sync="show">
+    <div class="result-number">{{value}} (TONT) = {{result}} (ONT)</div>
+    <el-input-number
+      v-model="value"
+      :step="1"
+      :controls="false"
+      :precision="2"
+      :min="0"
+      :max="10000"
+      style="width: 100%"
+    ></el-input-number>
+    <span slot="footer">
+      <el-button @click="close">{{$t('btn.cancel')}}</el-button>
+      <el-button type="primary" @click="submit">{{$t('btn.ok')}}</el-button>
+    </span>
+  </el-dialog>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { toFixed, multiple} from '@/utils/util'
+import { mapGetters } from "vuex";
+import { toFixed, multiple } from "@/utils/util";
+import { client } from "ontology-dapi";
+
+let Ont = require("ontology-ts-sdk");
 
 export default {
-    data() {
-        return {
-            show: false,
-            value: 0,
-            form: {
-                value: 0
+  data() {
+    return {
+      show: false,
+      value: 0,
+      form: {
+        value: 0
+      }
+    };
+  },
+  methods: {
+    open() {
+      this.value = 0;
+      this.show = true;
+    },
+    close() {
+      this.show = close;
+    },
+    submit() {
+        let TONT_DRGREE = 10 ^ 8
+        this.GetUserInfo().then(() => {
+        this.Withdraw(
+          this.userScriptHash,
+          this.value * 100000000
+        ).then(() => {
+          alert("1");
+        });
+      });
+      //this.close();
+    },
+    async GetUserInfo() {
+      let Crypto = Ont.Crypto;
+      let util = Ont.utils;
+      client.registerClient({});
+      const account = await client.api.asset.getAccount(); //得到当前插件钱包的用户地址。
+      console.log(account);
+      let Address = new Crypto.Address(account);
+      console.log(Address);
+      this.userAddress = Address.toString();
+      this.userScriptHash = Address.serialize();
+    },
+    async Withdraw(fromUserScriptHash, amount) {
+      console.log(amount)
+      const scriptHash = "95feeda3a7f41e43204353de64aa7b016e4ffaa3"; //合约的地址
+      const operation = "Withdraw"; //调用合约的方法名
+      const args = [
+        { type: "Bytearray", value: fromUserScriptHash },
+        { type: "Integer", value: amount }
+      ];
+      const gasPrice = 500;
+      const gasLimit = 200000;
+      client.api.smartContract
+        .invoke({
+          scriptHash,
+          operation,
+          args,
+          gasPrice,
+          gasLimit
+        })
+        .then(
+          result => {
+            setTimeout(() => {
+              console.log(2);
+              console.log(result);
+              console.log(result["transaction"]);
+              let txid = result["transaction"]; //这里需要判断一下，是否成功什么的。
+              client.api.network
+                .getSmartCodeEvent({
+                  value: txid.toString()
+                })
+                .then(
+                  err => {
+                    console.log(err); //得到交易的状态，json里面有个notify
+                    //取txlogjson文件中的最终信息，来看，结果，然后给界面上相应响应，主要看NOtify中对应的自己合约的东西。
+                    //对应自己的合约hash里面没有error的信息，就代表成功，
+                  },
+                  err => {
+                    console.log(err); //这里可能是网络原因或者交易还没被执行，还没有结果会到这里。
+                  }
+                );
+            }, 1000);
+          },
+          err => {
+            console.log(err);
+            if (err == "CANCELED") {
+              //用户在主动取消的，点击了cancel按钮的情况
             }
-        }
-    },
-    methods: {
-        open() {
-            this.value = 0;
-            this.show = true;
-        },
-        close() {
-            this.show = close;
-        },
-        submit() {
-            this.close();
-        }
-    },
-    computed: {
-        ...mapGetters([
-            'user',
-            'balance',
-            'loginStatus',
-            'btType',
-            'currencys'
-        ]),
-        result() {
-            return toFixed(multiple([this.value || 0, 2]));
-        }
+          }
+        );
     }
-}
+  },
+  computed: {
+    ...mapGetters(["user", "balance", "loginStatus", "btType", "currencys"]),
+    result() {
+      return toFixed(multiple([this.value || 0, 2]));
+    }
+  }
+};
 </script>
 
 <style lang="less">
 .result-number {
-    padding-bottom: 20px;
-    font-size: 20px;
-    text-align: center;
+  padding-bottom: 20px;
+  font-size: 20px;
+  text-align: center;
 }
 </style>
